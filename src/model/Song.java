e34bf0cac47ac4055683da70a387cbdcf8e53ad7
notan79 <njcrutchfield@gmail.com>
2025-02-21 19:53:13 -0700

    added comparator, and changes to libarymodel. Small changes to album.

--- src/model/Song.java
+++ src/model/Song.java
@@ -2,24 +2,36 @@ package model;
 
 public final class Song {
 	
-	private final String name;
+	private final String title;
 	private final String artist; 
 	private Rating rating;
+	private Album album;
+	private boolean isFavorite = false;
 
-	public Song(String name, String artist) {
-		this.name = name;
+	public Song(String title, String artist) {
+		this.title = title;
 		this.artist = artist; 
 		this.rating = Rating.NONE;
 	}
 	
 	public Song(Song song) {
-		this.name = song.name;
+		this.title = song.title;
 		this.artist = song.artist; 
+		this.rating = song.rating;
+		this.album = new Album(song.album);
+	}
+	
+	public static Song fromTitle(String title) {
+		return new Song(title, null);
+	}
+	
+	public static Song fromArtist(String artist) {
+		return new Song(null, artist);
 	}
 	
 	// Getters
-	public String getName() {
-		return name;
+	public String getTitle() {
+		return title;
 	}
 
 	public String getArtist() {
@@ -30,10 +42,27 @@ public final class Song {
 		return this.rating; 
 	}
 	
+	public Album getAlbum() {
+		return new Album(this.album);
+	}
+	
+	public boolean isFavorite() {
+		return this.isFavorite;
+	}
+	
+	// Setters
+	public void setFavorite() {
+		this.isFavorite = true;
+	}
+	
+	public void setRating(Rating rate) {
+		this.rating = rate;
+	}
+	
 	// toString
 	@Override
 	public String toString() {
-		return this.name + "_" + this.artist;
+		return this.title + " by " + this.artist;
 	}
 	
 	
