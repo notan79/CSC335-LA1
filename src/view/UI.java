2cc8598579ee8dc8ce518ad23a7e782eee6ae0e4
notan79 <njcrutchfield@gmail.com>
2025-02-27 13:24:22 -0700

    added UI comments

--- src/view/UI.java
+++ src/view/UI.java
@@ -15,6 +15,10 @@ public class UI {
 	private static MusicStore ms;
 	private static LibraryModel lib;
 
+	/*
+	 * 	Sets the user input scanner, creates music store and user library, 
+	 * 	calls the main prompt to start.
+	 */
 	public static void run() {
 		scanner = new Scanner(System.in);
 
@@ -24,6 +28,10 @@ public class UI {
 		mainPrompt();
 	}
 
+	/*
+	 * 	"Main Prompt," allows to move between store and library as well as
+	 * 	to exit from program.
+	 */
 	private static void mainPrompt() {
 		System.out.print("Choose an action (1-3):\n" + "1. Search Music Store.\n" + "2. Access User Library.\n"
 				+ "3. Exit.\n\n" + "Enter action: ");
@@ -41,6 +49,10 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Allows for any StoreFront object (music store and user library) to
+	 * 	search for artists, songs, albums from its songList.
+	 */
 	private static void searchPrompt(StoreFront s) {
 
 		// Same search actions in both user library and music store.
@@ -53,6 +65,7 @@ public class UI {
 				+ "4. Search for album by artist.\n" + "5. Exit search.\n\n" + "Enter action: ", tempString);
 		String inpString = runScannerOptions(5);
 
+		// Find Song by Title
 		if (inpString.equals("1")) {
 			System.out.print("Enter song title: ");
 			inpString = scanner.nextLine().strip();
@@ -64,7 +77,9 @@ public class UI {
 
 			for (Song song : tempArrayList)
 				System.out.println("- " + song);
-		} else if (inpString.equals("2")) {
+		} 
+		// Find song by artist
+		else if (inpString.equals("2")) {
 			System.out.print("Enter artist: ");
 			inpString = scanner.nextLine().strip();
 			ArrayList<Song> tempArrayList = s.findSongByArtist(inpString);
@@ -75,7 +90,9 @@ public class UI {
 
 			for (Song song : tempArrayList)
 				System.out.println("- " + song);
-		} else if (inpString.equals("3")) {
+		} 
+		// Find songs by album 
+		else if (inpString.equals("3")) {
 			System.out.print("Enter album title: ");
 			inpString = scanner.nextLine().strip();
 			ArrayList<Album> tempArrayList = s.findAlbumByTitle(inpString);
@@ -86,7 +103,9 @@ public class UI {
 
 			for (Album album : tempArrayList)
 				System.out.println("- " + album);
-		} else if (inpString.equals("4")) {
+		} 
+		// Find Albums by artist
+		else if (inpString.equals("4")) {
 			System.out.print("Enter artist: ");
 			inpString = scanner.nextLine().strip();
 			ArrayList<Album> tempArrayList = s.findAlbumByArtist(inpString);
@@ -113,12 +132,15 @@ public class UI {
 		searchPrompt(s);
 	}
 
-	// TODO: Nate implement
+	/*
+	 * 	Functionality for the user library.
+	 */
 	private static void libraryPrompt() {
 		System.out.printf("\nYou are in user library. Choose an action (1-8):\n" + "1. Search for songs or albums.\n"
 				+ "2. Add song or album to library.\n" + "3. Rate a song.\n" + "4. Favorite a song.\n"
 				+ "5. Get information for library.\n" + "6. Create playlist.\n"
 				+ "7. Add or remove song from playlist.\n" + "8. Exit User Library.\n\n" + "Enter action: ");
+		
 		String inpString = runScannerOptions(8);
 		if (inpString.equals("1")) {
 			searchPrompt(lib);
@@ -141,36 +163,50 @@ public class UI {
 			mainPrompt();
 			return;
 		}
+		// Stay in library
 		libraryPrompt();
 	}
 
+	/*
+	 * 	Gets the information from the library: albums, songs, playlists, favorites...
+	 */
 	private static void getLibraryInformation() {
 		String inpString;
 		System.out.printf("\nYou are in User library. Choose an action (1-5):\n" + "1. Show song titles.\n"
 				+ "2. Show artists.\n" + "3. Show albums.\n" + "4. Show playlists.\n" + "5. Show favorited songs.\n\n"
 				+ "Enter action: ");
 		inpString = runScannerOptions(5);
+		
+		// Get songs
 		if (inpString.equals("1")) {
 			ArrayList<Song> arrayList = lib.getSongList();
 			for (Song song : arrayList) {
 				System.out.println("- " + song);
 			}
-		} else if (inpString.equals("2")) {
+		} 
+		// Get artists
+		else if (inpString.equals("2")) {
 			ArrayList<String> arrayList = lib.getArtists();
 			for (String s : arrayList) {
 				System.out.println("- " + s);
 			}
-		} else if (inpString.equals("3")) {
+		} 
+		// Get albums
+		else if (inpString.equals("3")) {
 			ArrayList<String> arrayList = lib.getAlbums();
 			for (String s : arrayList) {
 				System.out.println("- " + s);
 			}
-		} else if (inpString.equals("4")) {
+		} 
+		// Get playlists
+		else if (inpString.equals("4")) {
 			ArrayList<String> arrayList = lib.getPlaylistsFormatted();
 			for (String s : arrayList) {
 				System.out.println("- " + s);
 			}
-		} else {
+		} 
+		// Get favorites
+		else {
 			ArrayList<String> arrayList = lib.getFavorites();
 			for (String s : arrayList) {
 				System.out.println("- " + s);
@@ -178,17 +214,22 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Prompt to add a given song or album to library
+	 */
 	private static void addToLibrary() {
 		String inpString;
 		System.out.printf("\nYou are adding from music store to user library. Choose an action (1-3):\n"
 				+ "1. Add song.\n" + "2. Add album.\n" + "3. Exit.\n\n" + "Enter action: ");
 		inpString = runScannerOptions(3);
+		
+		// Add song by title
 		if (inpString.equals("1")) {
 			System.out.print("\nEnter song title to add: ");
 			inpString = scanner.nextLine().strip();
 			ArrayList<Song> arr = ms.findSongByTitle(inpString);
 			if (arr.size() == 0) {
-				System.out.printf("No songs in music store with title: %s", inpString);
+				System.out.printf("No songs in music store with title: %s\n", inpString);
 			} else {
 				for (Song song : arr) {
 					System.out.printf("Add %s? (Enter Y for yes): ", song);
@@ -197,7 +238,9 @@ public class UI {
 						lib.addSong(song);
 				}
 			}
-		} else if (inpString.equals("2")) {
+		} 
+		// Add all songs from album
+		else if (inpString.equals("2")) {
 			System.out.print("\nEnter album title to add: ");
 			inpString = scanner.nextLine().strip();
 			ArrayList<Album> arr = ms.findAlbumByTitle(inpString);
@@ -217,6 +260,9 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Rate a given song
+	 */
 	private static void rateSongInLibrary() {
 		String inpString;
 
@@ -224,9 +270,13 @@ public class UI {
 		System.out.print("\nEnter song title to rate: ");
 		inpString = scanner.nextLine().strip();
 		ArrayList<Song> arr = lib.findSongByTitle(inpString);
+		
+		// No songs in the library with given name
 		if (arr.size() == 0) {
 			System.out.printf("No songs in library with title: %s", inpString);
-		} else {
+		} 
+		// Prompt which song to rate (could be repeat names)
+		else {
 			for (Song song : arr) {
 				System.out.printf("Rate %s: ", song);
 				inpString = scanner.nextLine().strip();
@@ -246,14 +296,21 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Favorite a song in the library
+	 */
 	private static void favoriteSong() {
 		String inpString;
 		System.out.print("\nEnter song title to favorite: ");
 		inpString = scanner.nextLine().strip();
 		ArrayList<Song> arr = lib.findSongByTitle(inpString);
+		
+		// No song with given name
 		if (arr.size() == 0) {
 			System.out.printf("No songs in library with title: %s\n", inpString);
-		} else {
+		} 
+		// Prompt which song to favorite (could be repeat names)
+		else {
 			for (Song song : arr) {
 				System.out.printf("Favorite %s? (Enter Y for yes): ", song);
 				inpString = scanner.nextLine().strip();
@@ -263,11 +320,16 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Creates a new playlist with inputted name
+	 */
 	private static void createPlaylist() {
 		String inpString;
 		System.out.print("Enter a name for your playlist: ");
 		inpString = scanner.nextLine().strip();
 		ArrayList<String> arr = lib.getPlaylists();
+		
+		// No repeat playlists
 		if (arr.contains(inpString))
 			System.out.printf("Already a playlist with name: %s\n", inpString);
 		else {
@@ -276,11 +338,16 @@ public class UI {
 		}
 	}
 
+	/*
+	 * 	Prompt to add or remove a song from a given playlist
+	 */
 	private static void addRemoveFromPlaylist() {
 		String inpString;
 
 		System.out.print("Choose playlist: ");
 		String playlist = scanner.nextLine().strip();
+		
+		// Make sure a playlist exists
 		boolean flag = false;
 		ArrayList<String> playlists = lib.getPlaylists();
 		for(String p : playlists) {
@@ -291,9 +358,11 @@ public class UI {
 			return;
 		}
 		
+		// Find to add or remove song
 		System.out.printf("\n" + "1. Add song.\n" + "2. Remove song.\n\n" + "Enter action: ");
-		
 		inpString = scanner.nextLine().strip();
+		
+		// Choose a song to add
 		if (inpString.equals("1")) {
 			System.out.print("Choose song title to add: ");
 			inpString = scanner.nextLine().strip();
@@ -302,7 +371,9 @@ public class UI {
 				System.out.printf("Unable to add %s to playlist: song not in library.\n", inpString);
 			}else
 				System.out.println("Song has been added.");
-		} else if (inpString.equals("2")) {
+		} 
+		// Choose a song to remove
+		else if (inpString.equals("2")) {
 			System.out.print("Choose song to remove: ");
 			inpString = scanner.nextLine().strip();
 			System.out.print("Choose artist for song remove: ");
@@ -311,6 +382,12 @@ public class UI {
 			System.out.println("Invalid input.");
 	}
 
+	/*
+	 * 	Helper method to guarantee a prompt (a number prompt for the different
+	 * 	options) is valid.
+	 * 
+	 * 	int options: the amount of options to choose from
+	 */
 	private static boolean promptValidity(String s, int options) {
 		try {
 			String.valueOf(s);
@@ -326,6 +403,11 @@ public class UI {
 		return false;
 	}
 
+	/*
+	 * 	Helper method to get inputs until a valid input is given (using promptValidity)
+	 * 
+	 * 	int options: the amount of options to choose from
+	 */
 	private static String runScannerOptions(int options) {
 		String inpString = "";
 		while (scanner.hasNextLine()) {
